6c49bfef0bf3a589c2f40b07cf6582a3424b3a7e
Guilherme Brust <gbrust15@gmail.com>
2023-07-12 20:58:07 -0300

    Criação usuario post /users + login do usuario /session

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,11 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 
 const router = Router();
 
-router.get('/test', (req: Request, res: Response) => {
-    return res.json({ message: 'Hello World' });
-});
+router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
 
 export { router }
