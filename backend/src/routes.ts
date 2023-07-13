723126ac9bb44efeb7fe1e55d573fb51347a0ae5
Guilherme Brust <gbrust15@gmail.com>
2023-07-13 13:03:24 -0300

    cria e lista categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,13 +4,20 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
+// ROTAS USER
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
-
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+// ROTAS CATEGORY
+router.post('/categories', isAuthenticated, new CreateCategoryController().handle)
+router.get('/categories', isAuthenticated, new ListCategoryController().handle)
 
 export { router }
