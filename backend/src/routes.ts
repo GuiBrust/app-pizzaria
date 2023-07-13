70527403a94487fb69be2dc6071d80eb324de6b6
Guilherme Brust <gbrust15@gmail.com>
2023-07-13 19:03:04 -0300

    cadastro produto (rota, serviço e controller)

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,8 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
@@ -19,5 +21,8 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 // ROTAS CATEGORY
 router.post('/categories', isAuthenticated, new CreateCategoryController().handle)
 router.get('/categories', isAuthenticated, new ListCategoryController().handle)
+
+// ROTAS PRODUCT
+router.post('/products', isAuthenticated, new CreateProductController().handle)
 
 export { router }
