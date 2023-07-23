dcf98b0c02679ea63437ed198ceaf6e38a6aeff9
Guilherme Brust <gbrust15@gmail.com>
2023-07-23 18:16:34 -0300

    implementação rotas privadas

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,5 +1,14 @@
+import { canSSRAuth } from "../../utils/canSSrAuth"
+
 export default function Dashboard() {
   return (
     <h1>Dashboard</h1>
   )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+  return {
+    props: {}
+  }
+})
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -12,7 +12,8 @@ import { toast } from 'react-toastify'
 import { AuthContext } from '../contexts/AuthContext'
 
 import Link from 'next/link'
-import { sign } from 'crypto'
+
+import { canSSRGuest } from '../utils/canSSRGuest'
 
 export default function Home() {
   const { signIn } = useContext(AuthContext)
@@ -81,3 +82,12 @@ export default function Home() {
     </>
   )
 }
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  
+
+
+  return {
+    props: {}
+  }
+})
