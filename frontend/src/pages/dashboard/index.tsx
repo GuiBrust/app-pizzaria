05452a7e1c3c3cd5c826a7dc803932e8bef5eac4
Guilherme Brust <gbrust15@gmail.com>
2023-07-30 13:55:56 -0300

    cria tela inicial de pedidos

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,20 +1,41 @@
 import { canSSRAuth } from "../../utils/canSSRAuth"
 import Head from 'next/head'
+import styles from './styles.module.scss'
 
 import { Header } from "../../components/ui/Header"
+import { FiRefreshCw } from 'react-icons/fi'
 
 export default function Dashboard() {
   return (
     <>
-    <Head>
-      <title>Painel - Pizzaria</title>
-    </Head>
-    <div>
-      <Header />
-      <h1>Painel</h1>
-    </div>
+      <Head>
+        <title>Painel - Pizzaria</title>
+      </Head>
+      <div>
+        <Header />
+
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Últimos pedidos</h1>
+            <button>
+              <FiRefreshCw size={25} color="#3fffa3" />
+            </button>
+          </div>
+
+          <article className={styles.listOrders}>
+
+            <section className={styles.orderItem}>
+              <button>
+                <div className={styles.tag}></div>
+                <span>Mesa xx</span>
+              </button>
+            </section>
+
+          </article>
+        </main>
+      </div>
     </>
-    )
+  )
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
